80a22dc9f864db9867c7972e8d9670b05445bc94
Thong Nguyen <nxthongbk@gmail.com>
2018-11-29 17:48:02 +0700

    updated deffered io

--- fbws.c
+++ fbws.c
@@ -13,17 +13,12 @@
 #include <linux/vmalloc.h>
 #include <linux/init.h>
 #include <linux/delay.h>
-
 #include <linux/uaccess.h>
 
 #include <linux/rmap.h>
 #include <linux/pagemap.h>
 
-
 #include "fbws.h"
-
-
-
 
 const unsigned char lut_full_update[] =
 {
@@ -68,10 +63,8 @@ static struct fb_var_screeninfo ourfb_var = {
 static int our_write(struct ourfb_par *par, u8 data)
 {
 	// u8 *ssbuf = par->ssbuf;
- // 	ssbuf[0] = data;
-
- // 	return spi_write(par->spi, ssbuf, 2);
-
+ 	// ssbuf[0] = data;
+ 	// return spi_write(par->spi, ssbuf, 2);
 
 	u8 txbuf[2];
 	txbuf[0] = data;
@@ -82,13 +75,8 @@ static void our_write_data(struct ourfb_par *par, u8 data)
 {
 	int ret = 0;
 	/* Set data mode */
-
-	
 	gpio_set_value(par->dc, 1);
-	//mdelay(1);
-	//udelay(100);
-	//printk("   Data: %02x\n",data);
-
+	
 	ret = our_write(par, data);
 	if (ret < 0)
 	{
@@ -96,8 +84,6 @@ static void our_write_data(struct ourfb_par *par, u8 data)
 		pr_err("%s: write data %02x failed with status %d\n",
 			par->info->fix.id, data, ret);
 	}
-	//mdelay(1);
-	
 }
 
 static int our_write_data_buf(struct ourfb_par *par,
@@ -105,7 +91,7 @@ static int our_write_data_buf(struct ourfb_par *par,
 {
 	/* Set data mode */
 	gpio_set_value(par->dc, 1);
-	mdelay(1);
+	
 	/* Write entire buffer */
 	return spi_write(par->spi, txbuf, size);
 }
@@ -118,13 +104,10 @@ static void our_write_cmd(struct ourfb_par *par, u8 data)
 	/* Set command mode */
 	gpio_set_value(par->dc, 0);
 	
-	//printk("Command: %02x\n",data);
-
 	ret = our_write(par, data);
 	if (ret < 0)
 		pr_err("%s: write command %02x failed with status %d\n",
 			par->info->fix.id, data, ret);
-	//mdelay(1);
 	
 }
 
@@ -133,12 +116,10 @@ static void our_write_cmd(struct ourfb_par *par, u8 data)
 */
 static void wait_until_idle(struct ourfb_par *par)
 {
-	//wait BUSY PIN HERE TBD
-
+	//wait Busy pin to LOW level
 	while(gpio_get_value(par->busy) != 0)
 	{
 		mdelay(100);
-
 	}
 }
 
@@ -193,8 +174,9 @@ static void our_run_cfg_script(struct ourfb_par *par)
 	} while (!end_script);
 }
 */
-
-//Reset display
+/**
+/*	@brief: Reset display
+*/
 static void our_reset(struct ourfb_par *par)
 {
 	/* Reset controller */
@@ -205,8 +187,177 @@ static void our_reset(struct ourfb_par *par)
 }
 
 
-//Update screen when mmap from user space
-//status: developing
+/**
+/*	@brief: Set Lut full update or partial update
+*/
+static void set_lut(struct ourfb_par *par,unsigned char* lut)
+{
+	 our_write_cmd(par,WS_WRITE_LUT_REGISTER);
+	 int i;
+	 for(i = 0;i < 30; i++)
+	 {
+ 		our_write_data(par,lut[i]);
+	 }
+}
+
+/**
+/*	@brief: Init Lut
+*/
+static int int_lut(struct ourfb_par *par,unsigned char* lut)
+{
+	
+	our_reset(par);
+
+    our_write_cmd(par,WS_DRIVER_OUTPUT_CONTROL);
+    our_write_data(par,(HEIGHT - 1) & 0xFF);
+    our_write_data(par,((HEIGHT - 1) >> 8) & 0xFF);
+    our_write_data(par,0x00);                     // GD = 0; SM = 0; TB = 0;
+    our_write_cmd(par,WS_BOOSTER_SOFT_START_CONTROL);
+    our_write_data(par,0xD7);
+    our_write_data(par,0xD6);
+    our_write_data(par,0x9D);
+    our_write_cmd(par,WS_WRITE_VCOM_REGISTER);
+    our_write_data(par,0xA8);                     // VCOM 7C
+    our_write_cmd(par,WS_SET_DUMMY_LINE_PERIOD);
+    our_write_data(par,0x1A);                     // 4 dummy lines per gate
+    our_write_cmd(par,WS_SET_GATE_TIME);
+    our_write_data(par,0x08);                     // 2us per line
+    our_write_cmd(par,WS_DATA_ENTRY_MODE_SETTING);
+    our_write_data(par,0x03);                     // X increment; Y increment
+    set_lut(par,lut);
+   
+    return 0;
+}
+
+
+/**
+/*	@brief: Set Memory Area
+*/
+static void set_memory_area(struct ourfb_par *par,int x_start,int y_start,int x_end,int y_end)
+{
+	our_write_cmd(par, WS_SET_RAM_X_ADDRESS_START_END_POSITION);
+	our_write_data(par, (x_start >> 3) & 0xFF);
+	our_write_data(par, (x_end >> 3) & 0xFF);
+
+	our_write_cmd(par, WS_SET_RAM_Y_ADDRESS_START_END_POSITION);
+	our_write_data(par,y_start & 0xFF);
+	our_write_data(par, (y_start >> 8) & 0xFF);
+	our_write_data(par,y_end & 0xFF);
+	our_write_data(par, (y_end >> 8) & 0xFF);
+
+}
+
+
+/**
+/*	@brief: Set Memory Pointer
+*/
+static void set_memory_pointer(struct ourfb_par *par,int x, int y)
+{
+	our_write_cmd(par, WS_SET_RAM_X_ADDRESS_COUNTER);
+	our_write_data(par, (x >> 3) & 0xFF);
+
+	our_write_cmd(par, WS_SET_RAM_Y_ADDRESS_COUNTER);
+	our_write_data(par, y & 0xFF);
+	our_write_data(par, (y >> 8) & 0xFF);
+	wait_until_idle(par);
+}
+
+/**
+/*	@brief: Clear Frame Memory
+*/
+static void clear_frame_memory(struct ourfb_par *par,unsigned char color)
+{
+	printk("call clear_frame_memory\n");
+	set_memory_area(par,0,0,WIDTH-1,HEIGHT-1);
+	int j;
+	for(j = 0; j < HEIGHT ;j++)
+	{
+		set_memory_pointer(par,0 , j);
+		our_write_cmd(par, WS_WRITE_RAM);
+		int i;
+		for(i = 0; i < WIDTH/8 ;i++)
+		{
+			our_write_data(par,color);			
+		}
+	}
+}
+
+/**
+/*	@brief: Set frame memory to update screen
+*/
+static void set_frame_memory(struct ourfb_par *par,unsigned char* image_buffer)
+{
+	set_memory_area(par, 0, 0, WIDTH-1, HEIGHT-1);
+	
+
+	int j;
+	for(j = 0; j < HEIGHT ; j++)
+	{
+		set_memory_pointer(par,0 , j);
+		our_write_cmd(par, WS_WRITE_RAM);
+
+		int i;
+		for(i = 0; i < (WIDTH-1)/8 ; i++)
+		{
+			our_write_data(par, image_buffer[i + j * (WIDTH/8)]);
+		}
+	}
+}
+
+/**
+/*	@brief: Display frame 
+*/
+static void display_frame(struct ourfb_par *par)
+{
+	our_write_cmd(par, WS_DISPLAY_UPDATE_CONTROL_2);
+	our_write_data(par, 0xC4);
+	our_write_cmd(par, WS_MASTER_ACTIVATION);
+	our_write_cmd(par, WS_TERMINATE_FRAME_READ_WRITE);
+	
+	wait_until_idle(par);
+}
+
+/**
+/*	@brief: Init display when module is loaded
+*/
+static int ourfb_init_display(struct ourfb_par *par)
+{
+	printk("ourfb_init_display is called\n");
+
+	/* TODO: Need some error checking on gpios */
+	gpio_request(par->rst, "sysfs"); 
+	gpio_request(par->dc, "sysfs");
+	gpio_request(par->busy, "sysfs");
+	        
+    gpio_direction_output(par->dc, true);   // Set the gpio to be in output mode and on
+   	gpio_set_value(par->dc, 0);          // Not required as set by line above (here for reference)
+   	gpio_export(par->dc, true);
+
+   	gpio_direction_output(par->rst, true);   // Set the gpio to be in output mode and on
+   	gpio_set_value(par->rst, 0);          // Not required as set by line above (here for reference)
+   	gpio_export(par->rst, true);
+
+
+   	gpio_direction_input(par->busy);   // Set the gpio to be in output mode and on
+   	gpio_set_value(par->busy, 0);          // Not required as set by line above (here for reference)
+   	gpio_export(par->busy, true);
+
+
+    if(int_lut(par,lut_full_update)!=0)
+	 {
+	 	printk("Init lut error");
+	 } 
+		
+	//clear screen	
+	clear_frame_memory(par,0xFF);
+	display_frame(par);
+
+	return 0;
+}
+
+/**
+/*	@brief: Update screen when mmap from user space
+*/
 static void ourfb_update_display(struct ourfb_par *par)
 {
 	
@@ -217,16 +368,17 @@ static void ourfb_update_display(struct ourfb_par *par)
 #ifdef __LITTLE_ENDIAN
 	printk("goto __LITTLE_ENDIAN\n");
  	int i;
-	u16 *vmem16 = (u16 *)vmem;
+	u8 *vmem8 = (u8 *)vmem;
 	//u16 *ssbuf = par->ssbuf;
  	u8 *ssbuf = par->ssbuf;
 
-
  	for (i=0; i<WIDTH*HEIGHT*BPP/8; i++)
  	{
- 		//ssbuf[i] = swab16(vmem16[i]);
- 		ssbuf[i] = 0xFF;
+ 		ssbuf[i] = vmem8[i];
  	}
+
+ 	set_frame_memory(par,ssbuf);
+ 	display_frame(par);
 
  #endif
 	/*
@@ -275,241 +427,13 @@ static void ourfb_update_display(struct ourfb_par *par)
 }
 
 
-static void set_lut(struct ourfb_par *par,unsigned char* lut)
-{
-	printk("call set lut\n");
-	 our_write_cmd(par,WS_WRITE_LUT_REGISTER);
-	 int i;
-	 for(i = 0;i < 30; i++)
-	 {
- 		our_write_data(par,lut[i]);
-	 }
-}
-
-
-//Init for e-ink display
-static int int_lut(struct ourfb_par *par,unsigned char* lut)
-{
-	
-	printk("Call init lut\n");
-	our_reset(par);
-
-    our_write_cmd(par,WS_DRIVER_OUTPUT_CONTROL);
-    our_write_data(par,(HEIGHT - 1) & 0xFF);
-    our_write_data(par,((HEIGHT - 1) >> 8) & 0xFF);
-    our_write_data(par,0x00);                     // GD = 0; SM = 0; TB = 0;
-    our_write_cmd(par,WS_BOOSTER_SOFT_START_CONTROL);
-    our_write_data(par,0xD7);
-    our_write_data(par,0xD6);
-    our_write_data(par,0x9D);
-    our_write_cmd(par,WS_WRITE_VCOM_REGISTER);
-    our_write_data(par,0xA8);                     // VCOM 7C
-    our_write_cmd(par,WS_SET_DUMMY_LINE_PERIOD);
-    our_write_data(par,0x1A);                     // 4 dummy lines per gate
-    our_write_cmd(par,WS_SET_GATE_TIME);
-    our_write_data(par,0x08);                     // 2us per line
-    our_write_cmd(par,WS_DATA_ENTRY_MODE_SETTING);
-    our_write_data(par,0x03);                     // X increment; Y increment
-    set_lut(par,lut);
-    /* EPD hardware init end */
-    return 0;
-}
-
-
-//Set memory area
-static void set_memory_area(struct ourfb_par *par,int x_start,int y_start,int x_end,int y_end)
-{
-	our_write_cmd(par, WS_SET_RAM_X_ADDRESS_START_END_POSITION);
-	our_write_data(par, (x_start >> 3) & 0xFF);
-	our_write_data(par, (x_end >> 3) & 0xFF);
-
-	our_write_cmd(par, WS_SET_RAM_Y_ADDRESS_START_END_POSITION);
-	our_write_data(par,y_start & 0xFF);
-	our_write_data(par, (y_start >> 8) & 0xFF);
-	our_write_data(par,y_end & 0xFF);
-	our_write_data(par, (y_end >> 8) & 0xFF);
-
-}
-
-
-//Set memeory pointer
-static void set_memory_pointer(struct ourfb_par *par,int x, int y)
-{
-	our_write_cmd(par, WS_SET_RAM_X_ADDRESS_COUNTER);
-	our_write_data(par, (x >> 3) & 0xFF);
-
-	our_write_cmd(par, WS_SET_RAM_Y_ADDRESS_COUNTER);
-	our_write_data(par, y & 0xFF);
-	our_write_data(par, (y >> 8) & 0xFF);
-	wait_until_idle(par);
-}
-
-
-//Clear frame memory by seting color
-static void clear_frame_memory(struct ourfb_par *par,unsigned char color)
-{
-	printk("call clear_frame_memory\n");
-	set_memory_area(par,0,0,WIDTH-1,HEIGHT-1);
-	int j;
-	for(j = 0; j < HEIGHT ;j++)
-	{
-		set_memory_pointer(par,0 , j);
-		our_write_cmd(par, WS_WRITE_RAM);
-		int i;
-		for(i = 0; i < WIDTH/8 ;i++)
-		{
-			//set color
-			our_write_data(par,color);
-			//printk("set %d,%d\n",i,j);
-		}
-	}
-}
-
-static void set_frame_memory(void)
-{
-	
-
-	//Will be developer to diplay image
-}
-
-static void display_frame(struct ourfb_par *par)
-{
-	printk("call display frame\n");
-	our_write_cmd(par, WS_DISPLAY_UPDATE_CONTROL_2);
-	our_write_data(par, 0xC4);
-	our_write_cmd(par, WS_MASTER_ACTIVATION);
-	our_write_cmd(par, WS_TERMINATE_FRAME_READ_WRITE);
-	
-	printk("End Write\n");
-
-
-	wait_until_idle(par);
-
-	printk("call display frame end\n");
-
-}
-
-
-//For testing update display
-static void ourfb_update_display1(struct ourfb_par *par)
-{
-	//Set memory Area
-	printk("Set memory Area\n");
-	int x_start = 0;
-	int y_start = 0;
-	int x_end = WIDTH-1;
-	int y_end = HEIGHT-1;
-
-	our_write_cmd(par, WS_SET_RAM_X_ADDRESS_START_END_POSITION);
-	our_write_data(par, (x_start >> 3) & 0xFF);
-	our_write_data(par, (x_end >> 3) & 0xFF);
-
-	our_write_cmd(par, WS_SET_RAM_Y_ADDRESS_START_END_POSITION);
-	our_write_data(par,y_start & 0xFF);
-	our_write_data(par, (y_start >> 8) & 0xFF);
-	our_write_data(par,y_end & 0xFF);
-	our_write_data(par, (y_end >> 8) & 0xFF);
-	//End Set Memory Area
-	
-	//Start set frame memory line by ine
-	printk("Start set frame memory line by line\n");
-	int j=0;
-	for (j = 0; j < HEIGHT; j++)
-	{
-		printk("j run time: %d\n",j);
-		//Set memory Pointer 0 j
-		our_write_cmd(par, WS_SET_RAM_X_ADDRESS_COUNTER);
-		our_write_data(par, (0 >> 3) & 0xFF);
-
-		our_write_cmd(par, WS_SET_RAM_Y_ADDRESS_COUNTER);
-		our_write_data(par, y_end & 0xFF);
-		our_write_data(par, (j >> 8) & 0xFF);
-		//Send command writeRAM
-		our_write_cmd(par, WS_WRITE_RAM);
-		int i = 0;
-		for(i = 0; i < WIDTH/8 ;i++)
-		{
-			//send color
-			our_write_data(par,0xFF);
-		}
-	}
-	//end set frame memory
-	//start display Frame
-	our_write_cmd(par, WS_DISPLAY_UPDATE_CONTROL_2);
-	our_write_data(par, 0xC4);
-	our_write_cmd(par, WS_MASTER_ACTIVATION);
-	our_write_cmd(par, WS_TERMINATE_FRAME_READ_WRITE);
-	printk("End Display and out\n");
-	wait_until_idle(par);
-	
-}
-
-
-//Init display when module is loaded
-static int ourfb_init_display(struct ourfb_par *par)
-{
-	printk("ourfb_init_display is called\n");
-
-	/* TODO: Need some error checking on gpios */
-	gpio_request(par->rst, "sysfs"); 
-	gpio_request(par->dc, "sysfs");
-	gpio_request(par->busy, "sysfs");
-	        
-    gpio_direction_output(par->dc, true);   // Set the gpio to be in output mode and on
-   	gpio_set_value(par->dc, 0);          // Not required as set by line above (here for reference)
-   	gpio_export(par->dc, true);
-
-   	gpio_direction_output(par->rst, true);   // Set the gpio to be in output mode and on
-   	gpio_set_value(par->rst, 0);          // Not required as set by line above (here for reference)
-   	gpio_export(par->rst, true);
-
-
-   	gpio_direction_input(par->busy);   // Set the gpio to be in output mode and on
-   	gpio_set_value(par->busy, 0);          // Not required as set by line above (here for reference)
-   	gpio_export(par->busy, true);
-
-
-
-    if(int_lut(par,lut_full_update)!=0)
-	 {
-	 	printk("Init lut error");
-	 } 
-	//  int k;
-	//  int u;
-
-	// for(u=0;u<100000;u++)
-	// {
-	// 	for(k=0; k < 100000;k++)
-	// 	{
-		
-	// 	our_write_data(par,0x00);
-
-	// 	//mdelay(1000);
-		
-	// 	our_write_cmd(par,0x55);
-	// 	//mdelay(1000);
-	// 	}
-	// }
-
-
-	
-
-	 clear_frame_memory(par,0xFF);
-	 display_frame(par);
-
-	// clear_frame_memory(par,0x00);
-	//display_frame(par);
-
-	return 0;
-}
-
 void ourfb_fillrect(struct fb_info *info, const struct fb_fillrect *rect)
 {
 	printk("ourfb_fillrect is called\n");
 
 	struct ourfb_par *par = info->par;
 
-	//sys_fillrect(info, rect);
+	sys_fillrect(info, rect);
 
 	ourfb_update_display(par);
 }
@@ -519,7 +443,7 @@ void ourfb_copyarea(struct fb_info *info, const struct fb_copyarea *area)
 	printk("ourfb_copyarea is called\n");
 	struct ourfb_par *par = info->par;
 
-	//sys_copyarea(info, area);
+	sys_copyarea(info, area);
 
 	ourfb_update_display(par);
 }
@@ -530,95 +454,18 @@ void ourfb_imageblit(struct fb_info *info, const struct fb_image *image)
 
 	struct ourfb_par *par = info->par;
 
-	//sys_imageblit(info, image);
+	sys_imageblit(info, image);
 
 	ourfb_update_display(par);
 }
 
-
-
-static ssize_t ourfb_read(struct fb_info *info, const char __user *buf,
-		size_t count, loff_t *ppos)
-{
-	printk("ourfb_read is called\n");
-
-	struct outfb_par *par = info->par;
-	unsigned long p = *ppos;
-	void *dst;
-	int err = 0;
-	unsigned long total_size;
-
-	if (info->state != FBINFO_STATE_RUNNING)
-		return -EPERM;
-
-	total_size = info->fix.smem_len;
-
-	if (p > total_size)
-		return -EFBIG;
-
-	if (count > total_size) {
-		err = -EFBIG;
-		count = total_size;
-	}
-
-	if (count + p > total_size) {
-		if (!err)
-			err = -ENOSPC;
-
-		count = total_size - p;
-	}
-
-	dst = (void __force *) (info->screen_base + p);
-
-	if (copy_from_user(dst, buf, count))
-		err = -EFAULT;
-
-	if  (!err)
-		*ppos += count;
-
-	ourfb_update_display(par);
-
-	return (err) ? err : count;
-}
-
-
+/**
+/*	@brief: udpate when get write action from user space
+*/
 static ssize_t ourfb_write(struct fb_info *info, const char __user *buf,
 		size_t count, loff_t *ppos)
 {
-	// printk("write from user space\n");
-	// struct outfb_par *par = info->par;
-	//ourfb_update_display(par);
-	//return 0;
 	
-	
-
-	
-
-
-	// struct outfb_par *par = info->par;
-	// our_write_cmd(par, WS_SET_RAM_X_ADDRESS_START_END_POSITION);
-
-
-	// printk("start update display\n");
-	// ourfb_update_display1(par);
-
-
-	//return 0;
-	// our_write_cmd(par, WS_SET_RAM_X_ADDRESS_START_END_POSITION);
-	// our_write_data(par, 0x00);
-	// our_write_data(par, 0x01);
-	// our_write_data(par, 0x02);
-	// our_write_data(par, 0x03);
-
-	// our_write_data(par, 0x04);
-	// our_write_data(par, 0x05);
-	// our_write_data(par, 0x06);
-	// our_write_data(par, 0x07);
-
-	// our_set_addr_win(par,0,0,WIDTH-1, HEIGHT-1);
-
-
-
 	unsigned long p = *ppos;
 	void *dst;
 	int err = 0;
@@ -658,55 +505,42 @@ static ssize_t ourfb_write(struct fb_info *info, const char __user *buf,
 	return (err) ? err : count;
 }
 
-
+/**
+/*	@brief: fb operator define
+*/
 static struct fb_ops ourfb_ops = {
 	.owner		= THIS_MODULE,
 	.fb_read	=fb_sys_read,
-	//.fb_read	= fb_sys_read,
-	//.fb_read	= ourfb_read,
 	.fb_write	= ourfb_write,
-	// .fb_fillrect	= ourfb_fillrect,
-	// .fb_copyarea	= ourfb_copyarea,
-	// .fb_imageblit	= ourfb_imageblit,
+	.fb_fillrect	= ourfb_fillrect,
+	.fb_copyarea	= ourfb_copyarea,
+	.fb_imageblit	= ourfb_imageblit,
 
-	.fb_fillrect	= cfb_fillrect,
-	.fb_copyarea	= cfb_copyarea,
-	.fb_imageblit	= cfb_imageblit,
+	//For default fb operator
+	// .fb_fillrect	= cfb_fillrect,
+	// .fb_copyarea	= cfb_copyarea,
+	// .fb_imageblit	= cfb_imageblit,
 };
 
+//Config Deferred IO
+static void ourfb_deferred_io(struct fb_info *info,
+				struct list_head *pagelist)
+{
+	ourfb_update_display(info->par);
+}
+static struct fb_deferred_io ourfb_defio = {
+	.delay		= HZ,
+	.deferred_io	= ourfb_deferred_io,
+};
 
-// static void ourfb_deferred_io(struct fb_info *info,
-// 				struct list_head *pagelist)
-// {
-// 	ourfb_update_display(info->par);
-// }
-// static struct fb_deferred_io ourfb_defio = {
-// 	.delay		= HZ,
-// 	.deferred_io	= ourfb_deferred_io,
-// };
+static void ourfb_deferred_io(struct fb_info *info,	struct list_head *pagelist)
+ {
+ 	ourfb_update_display(info->par);
+ } 
 
-// static void ourfb_deferred_io(struct fb_info *info,	struct list_head *pagelist)
-//  {
-//  	ourfb_update_display(info->par);
-//  } 
-
-// static struct fb_deferred_io ourfb_defio = {
-// 	.delay		= HZ,
-// 	.deferred_io	= ourfb_deferred_io,
-// };
-
-// static void hecubar_dpy__deferred_io(struct fb_info *info,	struct list_head *pagelist)
-//  {
-//  	ourfb_update_display(info->par);
-//  } 
-
-// static struct fb_deferred_io hecubafb_defio = {
-// 	.delay		= HZ,
-// 	.deferred_io	= hecubar_dpy__deferred_io,
-// };
-
-
-
+/**
+/*	@brief: Init FB SPI
+*/
 static int ourfb_spi_init(struct spi_device *spi)
 {
 	printk("start ourfb_spi_init\n");
@@ -735,13 +569,13 @@ static int ourfb_spi_init(struct spi_device *spi)
 		printk("Error in alloc:\n");
 		goto fballoc_fail;
 	}
-
 	
 	info->screen_base = (u8 __force __iomem *)vmem;
 	info->fbops = &ourfb_ops;
 	info->fix = ourfb_fix;
 	info->fix.smem_len = vmem_size;
 	info->var = ourfb_var;
+	//For color display (TBD)
 	/* Choose any packed pixel format as long as it's RGB565 */
 	info->var.red.offset = 11;
 	info->var.red.length = 5;
@@ -753,14 +587,8 @@ static int ourfb_spi_init(struct spi_device *spi)
 	info->var.transp.length = 0;
 	info->flags = FBINFO_FLAG_DEFAULT | FBINFO_VIRTFB;
 	
-	
-
-	//Init FBIO
-	// info->fbdefio = &hecubafb_defio;
-	// fb_deferred_io_init(info);
-	
-	// info->fbdefio = &ourfb_defio;
-	// fb_deferred_io_init(info);
+	info->fbdefio = &ourfb_defio;
+	fb_deferred_io_init(info);
 
 	par = info->par;
 	par->info = info;
@@ -770,7 +598,6 @@ static int ourfb_spi_init(struct spi_device *spi)
 	par->busy = pdata->busy_gpio;
 
 	
-
 #ifdef __LITTLE_ENDIAN
 	/* Allocate swapped shadow buffer */
 	vmem = vzalloc(vmem_size);
@@ -778,8 +605,6 @@ static int ourfb_spi_init(struct spi_device *spi)
 		return retval;
 	par->ssbuf = vmem;
 #endif
-
-
 
 	retval = register_framebuffer(info);
 	if (retval < 0)
@@ -830,14 +655,13 @@ static struct spi_device_id ourfb_spi_tbl[]={
 };
 
 
- MODULE_DEVICE_TABLE(spi,ourfb_spi_tbl);
+MODULE_DEVICE_TABLE(spi,ourfb_spi_tbl);
 
 
 static struct spi_driver ourfb_driver={
 	.driver={
 		.name 		=	"waveshare213",
 		.owner 		=THIS_MODULE,
-
 	},
 	
 	.id_table	=	ourfb_spi_tbl,
@@ -867,5 +691,5 @@ module_exit(ourfb_exit);
 MODULE_ALIAS("platform:eink");
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Thong Nguyen");
-MODULE_DESCRIPTION("E-Ink Display driver");
+MODULE_DESCRIPTION("FB Display driver");
 MODULE_VERSION("0.1");
